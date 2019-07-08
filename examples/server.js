ad1cf5fc6564ded7db19ac1887f57fdd062975da
Emre Ak <emre.akguel.1@gmail.com>
2019-07-08 16:11:56 +0200

    code refactoring

--- examples/server.js
+++ examples/server.js
@@ -1,7 +1,7 @@
 // Babel register for JSX
 require('babel-register')({
-  presets: ['es2015', 'react'],
-  extensions: [".es6", ".es", ".jsx", ".js"]
+    presets: ['es2015', 'react'],
+    extensions: [".es6", ".es", ".jsx", ".js"]
 });
 
 // Libraries
@@ -16,11 +16,11 @@ const http = require('http');
 
 // We need a function which handles requests and send response
 const handleRequest = (request, response) => {
-  // Render to string
-  const string = ReactDOMServer.renderToString(React.createFactory(Main)(), {});
+    // Render to string
+    const string = ReactDOMServer.renderToString(React.createFactory(Main)(), {});
 
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end(string);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(string);
 };
 
 // Create a server
@@ -28,7 +28,7 @@ const server = http.createServer(handleRequest);
 
 
 // Lets start our server
-server.listen(8000, function(){
-  // Callback triggered when server is successfully listening. Hurray!
-  console.log("Server listening on: http://localhost:%s", 8000);
+server.listen(8000, function () {
+    // Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", 8000);
 });
